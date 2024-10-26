aae7ee25f312aad2aa9374e92b6bedc3ac47d63d
Latko Nazar <crifh132@gmail.com>
2024-10-26 18:42:54 +0300

    Changed if else structure in order to use switch case as the next step

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -9,25 +9,18 @@ class GildedRose {
 
     public void updateQuality() {
         for (Item item : items) {
-            if (item.getName().equals("Aged Brie")
-                || item.getName().equals("Backstage passes to a TAFKAL80ETC concert")) {
-                    if (item.getQuality() < 50) {
-                        item.increaseQualityByOne();
-
-                        if (item.getName().equals("Backstage passes to a TAFKAL80ETC concert")) {
-                            if (item.getQuality() < 11) {
-                                item.increaseQualityByOne();
-                            }
-
-                            if (item.getQuality() < 6) {
-                                item.increaseQualityByOne();
-                            }
-                        }
-                    }
-                } else {
-                if (!item.getName().equals("Sulfuras, Hand of Ragnaros")) {
-                    item.decreaseQualityByOne();
+            if (item.getName().equals("Aged Brie")) {
+                item.increaseQualityByOne();
+            } else if (item.getName().equals("Backstage passes to a TAFKAL80ETC concert")) {
+                item.increaseQualityByOne();
+                if (item.getQuality() < 11) {
+                    item.increaseQualityByOne();
                 }
+                if (item.getQuality() < 6) {
+                    item.increaseQualityByOne();
+                }
+            } else if (!item.getName().equals("Sulfuras, Hand of Ragnaros")) {
+                item.decreaseQualityByOne();
             }
 
             if (!item.getName().equals("Sulfuras, Hand of Ragnaros")) {
